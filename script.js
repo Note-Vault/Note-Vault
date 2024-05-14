4f43946b1f19f09561e756bdff0aa580c3ddcf52
Shuvam Das <101086033+Cralsic123@users.noreply.github.com>
2024-05-14 10:03:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ mongoose
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// Middleware to verify JWT token and extract user ID
+const JWT_SECRET_KEY = process.env.JWTSECRETKEY;
+
+const isAuthenticated = (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
+    const { userId } = decoded;
+    User.findOne({ _id: userId })
+      .then((curr_user) => {
+        if (curr_user) {
+          req.userID = userId;
+          req.userNAME = curr_user.name;
+          next();
+        } else {
+          const errorMessage = "Internal server error";
+          res.render("error_template", { errorMessage });
+        }
+      })
+      .catch((error) => {
+        console.error("Server down", error);
+        const errorMessage = "An error occurred while token verification";
+        res.render("error_template", { errorMessage });
+      });
+  } else {
+    console.log("Cookie not found");
+    res.status(401).redirect("/");
+  }
+};
+
