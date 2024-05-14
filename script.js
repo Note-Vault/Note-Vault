52891eee2726a047e6cac600deaa9a005d430f06
Shuvam Das <101086033+Cralsic123@users.noreply.github.com>
2024-05-14 10:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ import Notebook from './models/Notebook'; // Import the Notebook model
 
 configDotenv();
 
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
