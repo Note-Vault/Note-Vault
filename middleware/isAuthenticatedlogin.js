7e7bb3b9516734208b580ed32cf631749f68fbd4
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-05-15 22:04:51 +0530

    middleware done

---
+++ middleware/isAuthenticatedlogin.js
@@ -0,0 +1,42 @@
+import { verify } from 'jsonwebtoken';
+
+const JWT_SECRET_KEY = process.env.JWTSECRETKEY;
+
+const isAuthenticatedlogin = (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+      // Verify and decode the token
+      console.log(token);
+      const decoded = verify(token, JWT_SECRET_KEY);
+      // Access the decrypted data from the token
+      const { userId } = decoded;
+      // Verifying the user ID with the database
+      User.findOne({ _id: userId })
+        .then((curr_user) => {
+          if (curr_user) {
+            console.log("User is Authorzied");
+            // setting the userID so that any authrized connection can use it
+            req.userID = userId;
+            req.userNAME = curr_user.name;
+            console.log(req.userNAME);
+            res.status(200).redirect("/note/show");
+          } else {
+            // res.send("Internal server error")
+            const errorMessage = "Internal server error";
+            res.render("error_template", { errorMessage });
+          }
+        })
+        .catch((error) => {
+          console.error("Server down", error);
+          // res.status(500).json({ message: 'An error occurred while token verification' });
+          const errorMessage = "An error occurred while token verification";
+          res.render("error_template", { errorMessage });
+        });
+    } else {
+      // Cookie does not exist
+      console.log("Cookie not found");
+      next();
+    }
+  };
+
+  export default isAuthenticatedlogin;
