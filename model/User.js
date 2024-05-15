4b768943ea0b1dab1dd95747577a5e4738dcf7f4
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-05-15 22:04:39 +0530

    model done

---
+++ model/User.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  });
+  
+modules.export = model("User", userSchema);
+  
